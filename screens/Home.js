b9b801421e31ab7b302e1051571a5ab91c110ce9
nirNearnir <94479026+nirkif@users.noreply.github.com>
2022-10-25 17:44:33 +0300

    backend/frontend
    frontend connected to backend

--- screens/Home.js
+++ screens/Home.js
@@ -1,32 +1,81 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
 // props - מידע זמני שפועל בזמן הפעלה ששומר ומבעביר מידע מדף לדף
+
+
 const Home = (props) => {
-    console.log('props'+JSON.stringify(props))
+    const [barberList , setBarberList] = useState([])
+    useEffect( ()=> {
+        const barberList = fetch('http://localhost:5988/allUsers',{
+            method: 'GET',// denpends upon your call POST or GET
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin':'*'
+            }
+          })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         setBarberList(responseJson)
+  
+      })
+      .catch((error) =>{
+          console.error(error);
+      })},[])
+    // async ()=>{
+    //      const getBarbers = async()=>{
+    //         const list = await fetch('http://localhost:5988/allUsers',{
+    //             method: 'GET',// denpends upon your call POST or GET
+    //             headers: {
+    //             Accept: 'application/json',
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Origin':'*'
+    //             }
+    //         })
+    //         console.log(JSON.stringify(list))
+    //         return list.json();
+    //      }
+    //             try{
+    //             let response =  getBarbers();
+    //             console.log(response)
+    //             setBarberList(response)
+    //             console.log(state.barberList)
+    //             }
+    //             catch(err){
+    //                 console.error(err);
+    //             }
+    // //         }
+    // },[]);
+    // console.log('props'+JSON.stringify(props))
 
-    fetch('http://147.235.196.240:5988/allUsers',{
-          method: 'GET',// denpends upon your call POST or GET
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          }
-        })
-    .then((response) => response.json())
-    .then((responseJson) => {
-
-       console.log(responseJson);
-
-    })
-    .catch((error) =>{
-        console.error(error);
-    });
+   
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Home Page</Text>
 
-            <TouchableOpacity onPress={ () => { props.navigation.navigate('Home')} } style={styles.buttonStyle}>   
+            <TouchableOpacity onPress={ () => { props.navigation.navigate('Profile')} } style={styles.buttonStyle}>   
                 <Text>Register to Site</Text>
             </TouchableOpacity>
+            <TouchableOpacity onPress={() => {console.log(barberList)}}><Text>click</Text></TouchableOpacity>
+
+
+            <FlatList
+                    data={barberList}//which data to use
+                    keyExtractor={barber => barber.username}//unique id for the item
+                    renderItem={barberDetails => //what will be shown from the item
+                    <TouchableOpacity onPress={() => {props.navigation.navigate('Profile',{barberDetails: barberDetails.item} )}}>
+                                  <View style={{backgroundColor:'fff',
+                                  width:'100%',
+                                  padding:22,
+                                  marginBottom:10,
+                                  borderRadius:10}}>
+                                    <Text>{barberDetails.item.username}</Text>
+                                  </View>
+                    </TouchableOpacity>
+                }/>
+
         </View>
     )
 
